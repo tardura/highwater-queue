0dbfce1efec6b1eec8723e0a75918929f63e73bb
tardura <tony.ardura@bti360.com>
2015-07-19 18:06:51 -0400

    Highwater Queue implemented with tests

--- src/main/java/HighwaterQueue.java
+++ src/main/java/HighwaterQueue.java
@@ -1,4 +1,79 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
 
-public class HighwaterQueue {
+public class HighwaterQueue<T> extends ArrayBlockingQueue<T> {
 
+    final ReentrantLock floodingLock;
+    private Consumer<T> floodOperation;
+
+   public HighwaterQueue(int highwaterMark, Consumer<T> floodOperation){
+        super(highwaterMark, true);
+        floodingLock = new ReentrantLock(true);
+        this.floodOperation = floodOperation;
+    }
+
+    private boolean reachedHighwaterMark() {
+        return remainingCapacity() == 0;
+    }
+
+    private void flashFlood(){
+        this.stream().forEach(floodOperation);
+        this.clear();
+    }
+
+    private void checkForAndProcessHighwaterMark(){
+        if(reachedHighwaterMark()){
+            flashFlood();
+        }
+    }
+
+    @Override
+    public boolean add(T t){
+        final ReentrantLock lock = this.floodingLock;
+        lock.lock();
+        try{
+            checkForAndProcessHighwaterMark();
+            return super.add(t);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public boolean offer(T t) {
+        final ReentrantLock lock = this.floodingLock;
+        lock.lock();
+        try{
+            checkForAndProcessHighwaterMark();
+            return super.offer(t);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void put(T t) throws InterruptedException {
+        final ReentrantLock lock = this.floodingLock;
+        lock.lock();
+        try{
+            checkForAndProcessHighwaterMark();
+            super.put(t);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public boolean offer(T t, long timeout, TimeUnit unit) throws InterruptedException {
+        final ReentrantLock lock = this.floodingLock;
+        lock.lock();
+        try {
+            checkForAndProcessHighwaterMark();
+            return super.offer(t, timeout, unit);
+        } finally {
+            lock.unlock();
+        }
+    }
 }
--- src/test/java/HighwaterQueueTest.java
+++ src/test/java/HighwaterQueueTest.java
@@ -1,12 +1,108 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 public class HighwaterQueueTest {
 
-    private HighwaterQueue highwaterQueue;
+    private HighwaterQueue<Integer> highwaterQueue;
+    private LinkedList<Integer> flushedValues;
 
     @Before
     public void setup(){
-
+        flushedValues = new LinkedList<Integer>();
+        highwaterQueue = new HighwaterQueue<Integer>(1, x -> flushedValues.add(x));
     }
+
+    @Test
+    public void normalInsertionWorksAsExpected(){
+        highwaterQueue.add(1);
+        assertThat(highwaterQueue.peek(), is(1));
+    }
+
+    @Test
+    public void insertionWithAFlushFlushesAsExpected(){
+        highwaterQueue.add(1);
+        highwaterQueue.add(2);
+        assertThat(highwaterQueue.peek(), is(2));
+        assertThat(highwaterQueue.contains(1), is(false));
+        assertThat(flushedValues.get(0), is(1));
+    }
+
+    @Test
+    public void insertionWithMultipleFlushesFunctionsAsExpected(){
+        highwaterQueue = new HighwaterQueue<Integer>(2, x-> flushedValues.add(x));
+        highwaterQueue.add(1);
+        highwaterQueue.add(2);
+        highwaterQueue.add(3);
+        assertThat(highwaterQueue.peek(), is(3));
+        assertThat(highwaterQueue.contains(1), is(false));
+        assertThat(highwaterQueue.contains(2), is(false));
+        assertThat(flushedValues.get(0), is(1));
+        assertThat(flushedValues.get(1), is(2));
+    }
+
+    @Test
+    public void normalOfferWorksAsExpected(){
+        highwaterQueue.offer(1);
+        assertThat(highwaterQueue.peek(), is(1));
+    }
+
+    @Test
+    public void offerWithMultipleFlushesFunctionsAsExpected(){
+        highwaterQueue = new HighwaterQueue<Integer>(2, x -> flushedValues.add(x));
+        highwaterQueue.offer(1);
+        highwaterQueue.offer(2);
+        highwaterQueue.offer(3);
+        assertThat(highwaterQueue.peek(), is(3));
+        assertThat(highwaterQueue.contains(1), is(false));
+        assertThat(highwaterQueue.contains(2), is(false));
+        assertThat(flushedValues.get(0), is(1));
+        assertThat(flushedValues.get(1), is(2));
+    }
+
+    @Test
+    public void normalPutWorksAsExpected() throws InterruptedException{
+        highwaterQueue.put(1);
+        assertThat(highwaterQueue.peek(), is(1));
+    }
+
+    @Test
+    public void putWithMultipleFlushesFunctionsAsExpected() throws InterruptedException {
+        highwaterQueue = new HighwaterQueue<Integer>(2, x -> flushedValues.add(x));
+        highwaterQueue.put(1);
+        highwaterQueue.put(2);
+        highwaterQueue.put(3);
+        assertThat(highwaterQueue.peek(), is(3));
+        assertThat(highwaterQueue.contains(1), is(false));
+        assertThat(highwaterQueue.contains(2), is(false));
+        assertThat(flushedValues.get(0), is(1));
+        assertThat(flushedValues.get(1), is(2));
+    }
+
+    @Test
+    public void normalOfferWithTimeoutWorksAsExpected() throws InterruptedException{
+        highwaterQueue.offer(1, 10l, TimeUnit.SECONDS);
+        assertThat(highwaterQueue.peek(), is(1));
+    }
+
+    @Test
+    public void offerWithTimeoutWithMultipleFlushesFunctionsAsExpected() throws InterruptedException{
+        highwaterQueue = new HighwaterQueue<Integer>(2, x -> flushedValues.add(x));
+        highwaterQueue.offer(1, 10l, TimeUnit.MINUTES);
+        highwaterQueue.offer(2, 10l, TimeUnit.MINUTES);
+        highwaterQueue.offer(3, 10l, TimeUnit.MINUTES);
+        assertThat(highwaterQueue.peek(), is(3));
+        assertThat(highwaterQueue.contains(1), is(false));
+        assertThat(highwaterQueue.contains(2), is(false));
+        assertThat(flushedValues.get(0), is(1));
+        assertThat(flushedValues.get(1), is(2));
+    }
+
 
 }
 
